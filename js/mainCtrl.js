9518979b3a161de0d570b428051ad54117c930fe
JT Parker <justin.parker@utahradiology.com>
2017-06-12 22:39:14 -0600

    places search wired up

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,12 +6,19 @@ app.controller('mainCtrl', ['$scope', 'mapService', function($scope, mapService)
   $scope.eatOptions.type = 'restaurant';
 
   $scope.pickDinner = function() {
+    var loc = new google.maps.LatLng(40.034378, -111.732246);
+    var dubJ = new google.maps.LatLng(40.609621, -111.983401);
+    $scope.eatOptions = {
+      location: dubJ,
+      radius: '10000',
+      types: ['restaurant']
+    }
     mapService.findEats($scope.eatOptions)
     .then(function(res) {
-      console.log(res);
+      $scope.yourDinner = res;
     })
+    .catch(console.error);
   };
-
 
   mapService.initMap();
 
--- js/mapService.js
+++ js/mapService.js
@@ -1,6 +1,5 @@
 app.service('mapService', ['$q', function($q) {
 
-
   this.initMap = function() {
       var initPlace = new google.maps.LatLng(41.8781, 87.6298);
 
@@ -8,22 +7,36 @@ app.service('mapService', ['$q', function($q) {
         center: initPlace,
         zoom: 200
       });
-      console.log(this.map);
   };
 
-  this.findEats - function(find) {
-    var d = $q.defer;
+  this.findEats = function(find) {
+    var d = $q.defer();
 
     this.searchEats = new google.maps.places.PlacesService(map);
     this.searchEats.nearbySearch(find, function(results, status) {
       if (status === 'OK') {
-        d.resolve(results)
-        console.log(results)
+        var picks = [];
+        var arr = results;
+        var max = arr.length;
+        for (var i = 0; i < arr.length; i++) {
+          var place = {};
+          place.name = arr[i].name;
+          // place.isOpen = arr[i].opening_hours.open_now;
+          place.price = arr[i].price_level;
+          picks.push(place);
+        }
+        var thePick = randNum(max);
+        d.resolve(picks[thePick]);
       } else {
         d.reject(status);
       }
     })
     return d.promise;
   };
+
+  function randNum(max) {
+    return Math.floor(Math.random() * (max - 0 + 1)) + 0;
+  };
+
 
 }]);
